a417e2e45a64d8dc00c67580bf52734a417a0d32
WinexWolf <anukriti.jaiswal01@gmail.com>
2023-10-21 15:55:09 -0700

    commented payment model

---
+++ payment/src/main/java/com/campushare/payment/model/Payment.java
@@ -0,0 +1,25 @@
+/* package com.campushare.payment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+@Document(collection = "payments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Payment {
+    @Id
+    private String paymentId; 
+    private String userId; // get user id from user service
+}
+
+
+
+ */
