5096eda69cca1d80bcbad389f33d4db6146d5002
Aishwarya Singh <aishwaryasingh0602@gmail.com>
2023-12-05 20:14:14 -0800

    update payment service tests

--- src/test/java/com/campushare/PaymentService/PaymentServiceTests.java
+++ src/test/java/com/campushare/PaymentService/PaymentServiceTests.java
@@ -1,22 +1,32 @@
 package com.campushare.PaymentService;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-import com.campushare.PaymentService.dto.User;
-import com.campushare.PaymentService.exception.CannotGetUserException;
+import com.campushare.PaymentService.dto.UsersRelatedPaymentDTO;
+import com.campushare.PaymentService.exception.OrderNotFoundException;
+import com.campushare.PaymentService.model.AccessTokenModel;
+import com.campushare.PaymentService.repository.AccessTokenRepository;
+import com.campushare.PaymentService.repository.PaymentRepository;
 import com.campushare.PaymentService.service.PaymentService;
+import com.campushare.PaymentService.service.PaypalSandbox;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class PaymentServiceTests {
     @Mock
     private RestTemplate restTemplate;
+
+    @Mock
+    private PaypalSandbox paypalSandbox;
+
+    @Mock
+    PaymentRepository paymentRepository;
+
+    @Mock
+    AccessTokenRepository accessTokenRepository;
 
     @InjectMocks
     private PaymentService paymentService;
@@ -25,18 +35,51 @@ public class PaymentServiceTests {
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
-    
-    public void testGetPayPalId() throws CannotGetUserException {
-        // 假设的User对象
-        User mockUser = new User();
-        mockUser.setAccount("mockedPayPalId");
 
-        // 配置RestTemplate模拟行为
-        when(restTemplate.getForEntity(anyString(), eq(User.class)))
-                .thenReturn(new ResponseEntity<>(mockUser, HttpStatus.OK));
+    @Test
+    void testCreatePayment_createsPayment() throws OrderNotFoundException {
+        String rideId = "rideId";
+        String driverId = "driverId";
+        String[] passengerIds = new String[1];
+        passengerIds[0] = "passenger1";
+        UsersRelatedPaymentDTO mockUserRelatedPaymentDTO = new UsersRelatedPaymentDTO(rideId, driverId, passengerIds);
+        String capturePaymentResponse = "capturePaymentResponse";
 
-        // 测试PaymentService的getPayPalId方法
-        String payPalId = paymentService.getPayPalId("mockUserId");
-        assertEquals("mockedPayPalId", payPalId);
+        String mockAccessToken = "mockAccessToken";
+        String mockAuthorizationId = "mockAuthorizationId";
+
+        AccessTokenModel mockAccessTokenModel = new AccessTokenModel("orderId", rideId, driverId, mockAccessToken, mockAuthorizationId, capturePaymentResponse);
+
+
+        when(accessTokenRepository.findByRideIdAndPassengerId(anyString(), anyString())).thenReturn(mockAccessTokenModel);
+        when(paypalSandbox.capturePayment(anyString(), anyString())).thenReturn(capturePaymentResponse);
+        when(paymentRepository.save(any())).thenReturn(null);
+
+        paymentService.createPayment(mockUserRelatedPaymentDTO);
+        verify(accessTokenRepository, times(3)).findByRideIdAndPassengerId(anyString(), anyString());
+        verify(paypalSandbox, times(1)).capturePayment(anyString(), anyString());
+        verify(paymentRepository, times(1)).save(any());
     }
+
+    @Test
+    void addCapturePaymentResponse_capturesResponse() throws OrderNotFoundException {
+        String rideId = "rideId";
+        String driverId = "driverId";
+        String passengerId = "passengerId";
+        String capturePaymentResponse = "capturePaymentResponse";
+
+        String mockAccessToken = "mockAccessToken";
+        String mockAuthorizationId = "mockAuthorizationId";
+
+        AccessTokenModel mockAccessTokenModel = new AccessTokenModel("orderId", rideId, driverId, mockAccessToken, mockAuthorizationId, capturePaymentResponse);
+
+        when(accessTokenRepository.findByRideIdAndPassengerId(rideId, passengerId)).thenReturn(mockAccessTokenModel);
+        when(accessTokenRepository.save(any())).thenReturn(null);
+
+        paymentService.addCapturePaymentResponse(rideId, passengerId, capturePaymentResponse);
+
+        verify(accessTokenRepository, times(1)).findByRideIdAndPassengerId(rideId, passengerId);
+        verify(accessTokenRepository, times(1)).save(any());
+    }
+
 }
